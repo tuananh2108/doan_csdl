10f0e6a243e089974209a8e6e4e81f3d10466256
tuananh2108 <htg2182000@gmail.com>
2021-09-25 15:21:26 +0700

    update 25/09/2021

--- partials/header.php
+++ partials/header.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Admin Dashboard </title>
+    <title>Admin Dashboard</title>
     <link rel="stylesheet" href="./vendor/owl-carousel/css/owl.carousel.min.css">
     <link rel="stylesheet" href="./vendor/owl-carousel/css/owl.theme.default.min.css">
     <link href="./vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet">
